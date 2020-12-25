3e3d6171f38be22eebf099bf0cc03aa1a96fe5b6
Admin <alexserghill@gmail.com>
2020-12-26 01:21:38 +0300

    make it better

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,17 +1,20 @@
 package com.example.demo;
 
-import com.example.demo.model.User;
-import com.example.demo.repository.UserRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.execute("CREATE TABLE jpa.users (id BIGSERIAL PRIMARY KEY, " +
+                "username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL)");
+
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 }
